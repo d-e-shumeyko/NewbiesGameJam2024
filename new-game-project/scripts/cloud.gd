aef612c04d28781cea5babb628da8c20860728e5
d-e-shumeyko <d.e.shumeyko@gmail.com>
2024-11-08 00:02:35 +0300

    added fixed audio, new assets, fixed enemy pathing, transitions

--- new-game-project/scripts/cloud.gd
+++ new-game-project/scripts/cloud.gd
@@ -1,15 +1,15 @@
-extends Node3D
+extends CharacterBody3D
 
 @export var area : Area3D
 @export var player : playerChara
 var target_position : Vector3
-@export var speed = 6
-@export var navAgent : NavigationAgent3D
+@export var speed = 6.0
+#@export var chara : CharacterBody3D
+
+func  _ready() -> void:
+	target_position = player.global_position
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,12 +17,21 @@ func _physics_process(delta: float) -> void:
 	look_at(player.global_position)
 	
 	
-	
-	#target_position = (player.global_position - position).normalized()
-	if (position.distance_to(player.global_position) > 0.1):
-		navAgent.target_position = player.global_position
-		
-		
+	if global_position != target_position:
+		velocity = Vector3( player.global_position.x - global_position.x, player.global_position.y - global_position.y, player.global_position.z - global_position.z) 
+		velocity = velocity * speed
+		target_position = player.global_position
 	
 	
 	
+	
+	move_and_slide()
+
+	
+	
+	
+
+
+func _on_area_3d_body_entered(body) -> void:
+	if body is playerChara:
+		get_tree().change_scene_to_file("res://scenes/level_cellar_lvl4.tscn")
---
+++ new-game-project/scripts/puzzle_trigger.gd
@@ -0,0 +1,5 @@
+extends Interactable
+
+
+func _on_interacted(body: Variant) -> void:
+	get_tree().change_scene_to_file("res://scenes/lvl3Puzzle.tscn")
