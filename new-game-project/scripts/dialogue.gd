e55f22251fd7747d67c997f37a53cf6ca7d4accc
d-e-shumeyko <d.e.shumeyko@gmail.com>
2024-11-08 22:08:53 +0300

    fixed dialogue in CI1

--- new-game-project/scripts/dialogue.gd
+++ new-game-project/scripts/dialogue.gd
@@ -1,9 +1,24 @@
 extends Interactable
 
 @export var text : Label
+@export var read : bool = false
+@export var played : bool = false
+@export var animator : AnimationPlayer
+@export var collider : Area3D
 
 func _ready() -> void:
 	text.hide()
 
 func _on_interacted(body: Variant) -> void:
-	text.show()
+	if read == false:
+		text.show()
+		read = true
+		if played == false:
+			animator.play("open_trapdoor")
+			played = true
+			collider.monitoring = true
+		
+	else:
+		text.hide()
+		read = false
+	
--- new-game-project/scripts/transitionCI1toLvl1.gd
+++ new-game-project/scripts/transitionCI1toLvl1.gd
@@ -2,4 +2,4 @@ extends Area3D
 func _on_body_entered(body):
 	if body is playerChara:
 		print("entered")
-		get_tree().change_scene_to_file("res://scenes/churchIntScene.tscn")
+		get_tree().change_scene_to_file("res://scenes/level_cellar_lvl1.tscn")
